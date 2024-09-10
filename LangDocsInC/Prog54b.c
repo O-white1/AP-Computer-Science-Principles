a5d4d630c4a8385811871ce8284191080b276806
Owen White <143815327+O-white1@users.noreply.github.com>
2024-09-10 15:16:01 +0000

    Prog54b done

---
+++ LangDocsInC/Prog54b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, num3, num4;
+    int sum;
+    float avg;
+
+    printf("Enter Number 1: "); scanf("%d", &num1);
+    printf("Enter Number 2: "); scanf("%d", &num2);
+    printf("Enter Number 3: "); scanf("%d", &num3);
+    printf("Enter Number 4: "); scanf("%d", &num4);
+
+    sum = num1 + num2 + num3 + num4;
+    avg = (float)sum / 4;
+
+    printf("Sum: %d \n", sum);
+    printf("Average: %f \n", avg);
+}
