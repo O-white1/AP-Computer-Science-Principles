6fd67a07bc70fd63d7d35ba4c98b078897a83426
Owen White <143815327+O-white1@users.noreply.github.com>
2024-09-13 15:22:05 +0000

    72t complete

--- LangDocsInC/Prog72t.c
+++ LangDocsInC/Prog72t.c
@@ -7,7 +7,7 @@ int main() {
     printf("Enter Time 1: "); scanf("%d", &time1);
     printf("Enter Time 2: "); scanf("%d", &time2);
 
-    hours =   abs(time1-time2) / 60;
+    hours =   abs(time1-time2) / 100;
     minutes = abs(time1-time2) % 60;
 
     printf("Time difference: %d hours, and %d minutes", hours, minutes);
