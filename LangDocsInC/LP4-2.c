c8289a57bf375a7d01d4290b885eb60415083db4
Owen White <143815327+O-white1@users.noreply.github.com>
2024-09-25 15:19:37 +0000

    LP4-2 actually done

--- LangDocsInC/LP4-2.c
+++ LangDocsInC/LP4-2.c
@@ -2,18 +2,17 @@
 
 int main() {
     const int     MAX_WEIGHT_KG = 27;
-    const float   MAX_VOL = 100000; // 100k
-    int           len, wid, height, volume, weight_kg;
-    char          error[] = "";
+    const int     MAX_VOL = 100000; // 100k
+    int           len, wid, height, weight_kg;
 
-    printf("Enter pkg weight in KG"); scanf("%d", &weight_kg);
-    printf("Enter pkg length in cm"); scanf("%d", &len);
-    printf("Enter pkg width in cm");  scanf("%d", &wid);
-    printf("Enter pkg height in cm"); scanf("%d", &height);
+    printf("Enter pkg weight in KG: "); scanf("%d", &weight_kg);
+    printf("Enter pkg length in cm: "); scanf("%d", &len);
+    printf("Enter pkg width  in cm: "); scanf("%d", &wid);
+    printf("Enter pkg height in cm: "); scanf("%d", &height);
 
-    volume = len*wid*height;
+    int volume = len*wid*height;
 
-    if (!volume > MAX_VOL && !weight_kg > MAX_WEIGHT_KG) {
+    if (!(volume > MAX_VOL) && !(weight_kg > MAX_WEIGHT_KG)) {
         printf("All good to ship pkg!");
     }
     else printf("pkg is either too big or too heavy to ship!");
