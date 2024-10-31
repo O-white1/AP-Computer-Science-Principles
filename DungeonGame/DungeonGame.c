336c74922466d125841f30c193470f985448aeaf
Owen White <143815327+O-white1@users.noreply.github.com>
2024-10-31 16:22:56 +0000

    qwertyyui

--- DungeonGame/DungeonGame.c
+++ DungeonGame/DungeonGame.c
@@ -1,30 +1,28 @@
-#include "GameHelper.h"
 #include <stdio.h>
+#include "GameHelper.h"
 
 int main() {
-    Enemy* Enemy;
-    Player* Player;
     SetUpPlayer();
     SetUpEnemy();
 
-    while (Player.Hp > 0) {
-        if (Player.Hp <= 0) break;
+    while (Player->Hp > 0) {
+        if (Player->Hp <= 0) break;
 
-        if (Enemy.Hp <= 0) {
-            Player.Score += 1;
+        if (Enemy->Hp <= 0) {
+            Player->Score += 1;
             SetUpEnemy();
             PromptAndChoice();
         }
         else PromptAndChoice();
         PromptAndChoice();
-        if (Player.Choice==0) Enemy.Hp -= Player.Dmg;
-        if (Player.Choice==1) {
-            Player.Hp += RandHp(Player.Hp);
+        if (Player->Choice==0) Enemy->Hp -= Player->Dmg;
+        if (Player->Choice==1) {
+            Player->Hp += RandHp(Player->Hp);
         }
-        if (Player.Choice==2) {
+        if (Player->Choice==2) {
             printf("Coward!\n");
             break;
         }
     }
-    printf("You Died\nScore: %d\n", Player.Score);
+    printf("You Died\nScore: %d\n", Player->Score);
 }
--- DungeonGame/GameHelper.h
+++ DungeonGame/GameHelper.h
@@ -1,5 +1,5 @@
-typedef struct Enemy() {
-    char Name[] = "";
+typedef struct Enemy {
+    *char Name = "";
     int  Hp = 0;
     int  Dmg = 0;
     char EnemyNames[][] = {
@@ -7,7 +7,7 @@ typedef struct Enemy() {
         "Dragon",
         "Vampire",
         "Giant Rat"
-    }
+    };
 };
 
 typedef struct Player {
@@ -21,26 +21,26 @@ int RandHp(int limit)  {return rand()%limit;}
 int RandDmg(int limit) {return rand()%limit;}
 
 void ReviveEnemy() {
-    Enemy.Name = EnemyNames[rand()%4][];
-    Enemy.Hp  = RandHp(50);
-    Enemy.Dmg = RandDmg(10);
+    Enemy->Name = EnemyNames[rand()%4];
+    Enemy->Hp  = RandHp(50);
+    Enemy->Dmg = RandDmg(10);
 }
 
 void SetUpPlayer() {
-    Player.Hp  = RandHp(200);
-    Player.Dmg = RandDmg(20);
-    Player.Score = 0;
-    Player.Choice = 0;
+    Player->Hp  = RandHp(200);
+    Player->Dmg = RandDmg(20);
+    Player->Score = 0;
+    Player->Choice = 0;
 }
 
 void SetUpEnemy() {
-    Enemy.Name  = EnemyNames[rand()%4][];
-    Enemy.Hp    = RandHp(50);
-    Enemy.Dmg   = RandDmg(10);
+    Enemy->Name  = EnemyNames[rand()%4];
+    Enemy->Hp    = RandHp(50);
+    Enemy->Dmg   = RandDmg(10);
 }
 
 void PromptAndChoice() {
     printf("You see a %s with %d Health and %d attack power!\n", Enemy.Name, Enemy.Hp, Enemy.Dmg);
     printf("What Do you do?\tAttack(0)\tHeal(1)\tFlee(2)");
-    scanf("%d", &Player.Choice);
+    scanf("%d", &Player->Choice);
 }
