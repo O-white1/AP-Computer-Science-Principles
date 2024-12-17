e686d4a1b5a59356dba1dba965643f5ab769ecd8
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-12-16 19:14:58 -0600

    qweret

---
+++ TodoList(Project2)/Item.java
@@ -0,0 +1,11 @@
+public static class Item {
+    int    myPriority = 0;
+    String myName = 0;
+    String myDate = 0;
+
+    public void Item(int priority, String name, String date) {
+        myPriority = priority;
+        myName = name;
+        myDate = date;
+    }
+}
