b0b702f6e799afa5695bd40f7bde956fa3671906
Owen White <143815327+O-white1@users.noreply.github.com>
2025-01-30 16:31:43 +0000

    1/30/25 (10:31) [925b part 1 complete, part 2 TODO]

---
+++ LangDocsInC/prog925b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    //part 1
+    int a = 29;
+    int b = 15;
+    printf("%d\n", a&b);
+    printf("%d\n", a|b);
+    printf("%d\n", a^b);
+    printf("%d\n", ~a);
+    printf("%d\n", a<<2);
+    printf("%d\n", b>>2);
+    //part 2
+
+}
