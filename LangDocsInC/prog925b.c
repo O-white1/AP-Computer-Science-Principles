03ed5a964042409f497007542c2a06e5d7b8d38a
Owen White <143815327+O-white1@users.noreply.github.com>
2025-02-14 16:58:50 +0000

    prog985t and prog925b done

--- LangDocsInC/prog925b.c
+++ LangDocsInC/prog925b.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
 
 int main() {
-    //part 1
     int a = 29;
     int b = 15;
-    printf("%d\n", a&b);
-    printf("%d\n", a|b);
-    printf("%d\n", a^b);
-    printf("%d\n", ~a);
-    printf("%d\n", a<<2);
-    printf("%d\n", b>>2);
-    //part 2
+    int c;
 
+    c = a & b;
+    printf("%d\n", c);
+
+    c = a | b;
+    printf("%d\n", c);
+
+    c = a ^ b;
+    printf("%d\n", c);
+
+    c = ~a;
+    printf("%d\n", c);
+
+    c = a << 2;
+    printf("%d\n", c);
+
+    c = b >> 2;
+    printf("%d\n", c);
+
+    int x = 0b11001010;
+    x &= 0b11000010;
+    printf("%d\n", x);
+
+    x |= 0b00000100;
+    printf("%d\n", x);
+
+    x ^= 0b01000000;
+    printf("%d\n", x);
+
+    x &= 0b00011100;
+    int y = x >> 2;
+    printf("%d\n", y);
+
+    int m = 5;
+    int n = 9;
+    int packed = (n << 8) | m;
+    printf("%d\n", packed);
 }
