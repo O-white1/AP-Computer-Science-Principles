6d615f2d41294150039aa57365ddef84f53cc3c2
Owen White <143815327+O-white1@users.noreply.github.com>
2025-03-21 11:23:28 -0500

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -2,28 +2,32 @@ import java.util.Random;
 
 public abstract class Entity {
     protected Random myRandom = new Random();
+    protected boolean isWinner = true;
     protected String myName;
     protected int myHp;
+
 
     public Entity(int hp) {
         this.myHp = hp;
     }
-
+    public void Heal(int hp) {
+        myHp +=  hp;
+    }
     public void TakeDamage(int dmg) {
         myHp -= dmg;
         if (myHp <= 0) {
             System.out.println(myName + " has died");
+            isWinner = false;
         }
         else {
             System.out.println(myName + " has " + myHp + " health left");
         }
     }
 
-    public void   Heal(int hp)            {myHp +=  hp;}
-    public void Attack(Entity e, int dmg) {e.TakeDamage(dmg);}
 
+    public void Attack(Entity e, int dmg) {e.TakeDamage(dmg);}
     public String getName() {return myName;}
-    public int      getHp() {return myHp;}
+    public int getHp() {return myHp;}
 
 
 }
