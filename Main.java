5214c843ebc35b9a61365d65081b06c38116dfc5
Owen White <143815327+O-white1@users.noreply.github.com>
2025-03-21 11:23:53 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,28 +3,46 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<Enemy> dead = new ArrayList<>();
-        Scanner sc  = new Scanner(System.in);
-        Random rand = new Random();
+    static Random rand = new Random();
+    static Scanner sc  = new Scanner(System.in);
+    static ArrayList<Enemy> dead = new ArrayList<>();
+    static Player player = new Player("TEMP", 999);
+    static int enemyType = rand.nextInt(1, 3);
 
-        System.out.print("Enter Name: ");String name = sc.nextLine();
-        Player player = new Player(name, 100);
-
-        int enemyType = rand.nextInt(1, 3);
-
-        if (enemyType==1) {
+    public static void fightCycle() {
+        enemyType = rand.nextInt(1, 3);
+        if (enemyType == 1) {
             Slime slime = new Slime(rand.nextInt(20, 30));
             slime.Fight(player);
+            if (player.isWinner) {
+                dead.add(slime);
+                fightCycle();
+            }
         }
-        if (enemyType==2) {
+        if (enemyType == 2) {
             Goblin goblin = new Goblin(rand.nextInt(25, 50));
             goblin.Fight(player);
+            if (player.isWinner) {
+                dead.add(goblin);
+                fightCycle();
+            }
         }
-        if (enemyType==3) {
+        if (enemyType == 3) {
             Wizard wizard = new Wizard(rand.nextInt(30, 75));
             wizard.Fight(player);
+            if (player.isWinner) {
+                dead.add(wizard);
+                fightCycle();
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter Name: ");String name = sc.nextLine();
+        player = new Player(name, 100);
+        fightCycle();
+
+
 
     }
 }
