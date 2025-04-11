b9fb74c935cc3c8d53806e35c7b2caef0c59a786
Owen White <white.o3@stu.janesville.k12.wi.us>
2025-04-11 10:32:58 -0500

    4/11/2025: working on 2017_6

--- LangDocsInC/MSOE_2017_6_PY.py
+++ LangDocsInC/MSOE_2017_6_PY.py
@@ -1,30 +1,20 @@
-output: int = 0
-def isPrimeFactor(n, number) -> bool:
-    prime = True
-    for i in range(2, n):
-        if (n % i == 0) and (i != n):
-            prime = False
-    factor = False
-    for x in range (1, n):
-        if (x*n==number):
-            factor = True
-
-    if factor and prime: return True
-    return False
-
-
-
-
-
-
-
 def main() -> None:
-    num = int(input("Enter Number"))
-    for i in range(0, num):
-        if isPrimeFactor(num, i):
-            pass
+    x = int(input("Enter positive int 1-1000: "))
+    print("the Dedekind Sci function output is " + str(dSci(x)))
 
-    print("the output of the Dedekind ψ (psi) function for this number is: " + str(out))
+def dSci(n) -> int:
+    if (n==1): return 1
+    out = n
+    for i in range(2, n):
+        if (n%i==0):
+            out *= (i+1)/i
+            while ((n % i)==0):
+                out = out/i
+
+    if (n>1):
+        out = out *(n+1)/n
+    return out
+
 
 if __name__ == "__main__":
     main()
